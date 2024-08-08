eb372a074bd664c039c1ac0f6e1c3fb4a8a5e391
Limber Sanchez <limbersanchezmendoza@gmail.com>
2024-08-07 23:08:22 -0400

    Sesion 3 070824

--- BlazorPeliculas/Client/obj/Debug/net7.0/BlazorPeliculas.Client.AssemblyInfo.cs
+++ BlazorPeliculas/Client/obj/Debug/net7.0/BlazorPeliculas.Client.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorPeliculas.Client")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+777f144ae37100635a0fe00f89f48fbd3036c7a0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a7497be3183e4604342e24c14d6d5c2733f28cbd")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorPeliculas.Client")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorPeliculas.Client")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
